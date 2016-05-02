71acd581f9a893c077b6bab6d1e0409b076d63c0
RoeiAvraham <roeiavraham33@gmail.com>
2016-05-02 21:23:47 +0300

    frontend signup

--- ez_plant/app.py
+++ ez_plant/app.py
@@ -1,27 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 import os.path
+from user import User
+from plant import Plant
 import sys
 
 sys.path.append(os.path.dirname(__file__))
-from user import User
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return jsonify(username="shibi", email="shibi@gmail.com")
+def root():
+    return render_template('index.html')
+    # Comment for github
+    #return jsonify(username="shibi", email="shibi@gmail.com")
     # return('Hello World')
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    user = User(data['email'], data['password'])
+    user.save_to_database()
+
+    print(data['email'])
+    print(data['userName'])
+    print(data['password'])
+    return jsonify(success="true")
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
+
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
