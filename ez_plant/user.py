0010f2924b4edddb8b1228b6fa2f11be9ff9c994
RoeiAvraham <roeiavraham33@gmail.com>
2016-05-17 23:32:53 +0300

    Revert "flask login"
    This reverts commit 9ab62a4482ab29ead4f11806a33fc1d7c476a4f5.

--- ez_plant/user.py
+++ ez_plant/user.py
@@ -1,25 +1,22 @@
 import uuid
 from ez_plant.mongo_handler import MongoHandler
-from flask.ext.login import UserMixin
+from ez_plant.hashing_handler import HashingHandler
 
-class User(UserMixin):
-    def __init__(self, email, password, first_name, last_name):
+class User(object):
+    def __init__(self, email, password, first_name="Anat", last_name="Eliyahu"):
+        self.user_id = uuid.uuid4().hex
         self.username = email
+        self.email = email
         self.first_name = first_name
         self.last_name = last_name
         self.plants = []
-        self.password = password
+        self.password = ""
+        self._encrypt_password(password)
+
+    def _encrypt_password(self, password):
+        hashing_handler = HashingHandler()
+        self.password = hashing_handler.encrypt(password)
 
     def save_to_database(self):
         mongo_worker = MongoHandler()
         mongo_worker.insert_object(vars(self), 'users')
-
-    @classmethod
-    def get_from_database(self, username):
-        mongo_worker = MongoHandler()
-        user_doc = mongo_worker.get_single_object('users', { "username": username })
-        return user_doc
-
-    # override
-    def get_id(self):
-        return self.username
