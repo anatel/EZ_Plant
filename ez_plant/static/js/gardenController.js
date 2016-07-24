0196a9c98acce350ba0b51e0715d651ae785a61c
Yulia <yulia@Yulias-Air.home>
2016-07-24 22:31:12 +0300

    added 4 plants

--- ez_plant/static/js/gardenController.js
+++ ez_plant/static/js/gardenController.js
@@ -1,8 +1,18 @@
 var ez_plant = angular.module('ez_plant');
 
-ez_plant.controller('gardenController', ['$scope', 'AuthService', function($scope, AuthService) {
+
+ez_plant.controller('gardenController', ['$scope', 'AuthService', '$rootScope', function($scope, AuthService, $rootScope) {
   $scope.userDetails = AuthService.getUser();
-  var plantsString = [{"name":"Plant1","type":"Rose","port_number":"A1","img_url":"static/assets/images/red-rose-plant.png","water_data":{"last_watered":"Sat Jul 23 2016 16:39:47"}},{"name":"Plant2","type":"Cactus","port_number":"A0","img_url":"static/assets/images/img-thing.jpg","water_data":{"last_watered":"Sat Jul 23 2016 16:50:47"}}];
+  var plantsString = [{"name":"Plant1","type":"Rose","port_number":"A1","img_url":"static/assets/images/red-rose-plant.png","water_data":{"last_watered":"23/07/16 16:39"}},{"name":"Plant2","type":"Cactus","port_number":"A0","img_url":"static/assets/images/img-thing.jpg","water_data":{"last_watered":"23/07/17 17:00"}},{"name":"Plant3","type":"Orchid","port_number":"A2","img_url":"","water_data":{"last_watered":"23/07/17 17:00"}},{"name":"Lilu","type":"Lilac","port_number":"A4","img_url":"static/assets/images/lilac.jpg","water_data":{"last_watered":"23/07/18 17:00"}}];
   $scope.plants = plantsString;
   console.log($scope.plants);
+
+  $scope.openPlantDetails = function(plantIndex){
+    $rootScope.plant = $scope.plants[plantIndex];
+  };
+
+}]);
+
+ez_plant.controller('plantController', ['$scope', '$rootScope', function($scope, $rootScope) {
+
 }]);
---
+++ ez_plant/static/js/plantController.js
@@ -0,0 +1,5 @@
+var ez_plant = angular.module('ez_plant');
+
+ez_plant.controller('plantController', ['$scope', '$rootScope' function($scope, $rootScope) {
+
+}]);
