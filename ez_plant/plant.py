f98b996ba3a2b315daeadb8fbbd493746c57596e
Yulia <yulia@Yulias-Air.home>
2016-07-30 16:15:24 +0300

    added functions to plant.py

--- ez_plant/plant.py
+++ ez_plant/plant.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class WaterMode(Enum):
-    TIME = 0
-    MOISTURE = 1
+    TIME = "schedule"
+    MOISTURE = "moisture"
 
 class Plant(object):
     def __init__(self, name, water_data):
@@ -16,13 +16,28 @@ class Plant(object):
             self.water_mode = water_data["water_mode"]
             if self.water_mode == WaterMode.MOISTURE.value:
                 self.low = water_data["low_threshold"]
-                self.high = water_data["high_threshold"]
+                #TODO add test if value is ok (if not empty of if exeed range)
+                # self.high = water_data["high_threshold"]
             elif self.water_mode == WaterMode.TIME.value:
-                self.repeats = water_data["repeats"]
+                # self.repeats = water_data["repeats"]
                 self.repeat_every = water_data["repeat_every"]
-                self.repeat_on = water_data["repeat_on"]
-                self.start_on = water_data["start_on"]
+                self.hour = water_data["hour"]
+                # self.start_on = water_data["start_on"]
             else:
                 return True
                 #raise exception
             self.last_watered = None
+
+    def save_to_database(self):
+        mongo_worker = MongoHandler()
+        mongo_worker.insert_object(vars(self), 'plants')
+
+    @classmethod
+    def get_from_database(self, plantid):
+        mongo_worker = MongoHandler()
+        plant_doc = mongo_worker.get_single_object('plants', { "id": plantid })
+        return user_doc
+
+    # override
+    def get_id(self):
+        return self.id
