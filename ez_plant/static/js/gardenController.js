d130fc485a7c18382408786a22502bad8f1b7cec
Anat <anat.eliahu@gmail.com>
2016-07-30 11:20:44 +0300

    add pic to form

--- ez_plant/static/js/gardenController.js
+++ ez_plant/static/js/gardenController.js
@@ -6,7 +6,6 @@ ez_plant.controller('gardenController', ['$scope', 'AuthService', '$rootScope', 
   var plantsString =[{"plant_id":"a","name":"Plant1","type":"Rose","port_number":"A1","img_url":"static/assets/images/red-rose-plant.png","water_data":{"last_watered":"23/07/16 16:39"}},{"plant_id":"b","name":"Plant2","type":"Cactus","port_number":"A0","img_url":"static/assets/images/img-thing.jpg","water_data":{"last_watered":"23/07/17 17:00"}},{"plant_id":"c","name":"Lilu","type":"Lilac","port_number":"A4","img_url":"static/assets/images/lilac.jpg","water_data":{"last_watered":"23/07/18 17:00"}}];
   $scope.plants = plantsString;
   $scope.showForm = false;
-  console.log($scope.plants);
 
   $scope.openPlantDetails = function(plantIndex){
     $scope.showForm = true;
@@ -18,8 +17,21 @@ ez_plant.controller('gardenController', ['$scope', 'AuthService', '$rootScope', 
     }, 2000);
   };
 
+  $scope.openDialog = function() {
+    $("#inputId").click();
+  };
 
+  $scope.readURL = function(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
 
+      reader.onload = function(e) {
+        $('.imgContainer img')
+          .attr('src', e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
 
   function placeArrow()
   {
