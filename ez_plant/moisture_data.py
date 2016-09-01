e6278516c56248f592653e3b6712b813129df373
RoeiAvraham <roeiavraham33@gmail.com>
2016-09-01 23:20:00 +0300

    moisture percentage is calculated and saved in database

--- ez_plant/moisture_data.py
+++ ez_plant/moisture_data.py
@@ -1,6 +1,8 @@
 from ez_plant.mongo_handler import MongoHandler
 import datetime
 
+ARDUINO_MOISTURE_MIN_VALUE = 1024
+
 class MoistureData(object):
     def __init__(self, username, plant_id, moisture, timestamp=None):
         self.username = username
@@ -11,11 +13,11 @@ class MoistureData(object):
     def save_to_database(self):
         mongo_worker = MongoHandler()
         moisture_stats_object = {}
-        moisture_stats_object['moisture'] = int(self.moisture)
+        moisture_stats_object['moisture'] = self.moisture
         moisture_stats_object['timestamp'] = self.timestamp
         mongo_worker.add_doc_to_nested_array('moisture_stats',
                                             { "username": self.username, "plants.plant_id": self.plant_id },
                                             'plants', 'stats', moisture_stats_object)
 
     def moisture_value_to_percentage(self, moisture):
-        return moisture
+        return float((abs(float(moisture) - ARDUINO_MOISTURE_MIN_VALUE) / ARDUINO_MOISTURE_MIN_VALUE) * 100)
